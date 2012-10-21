56e51c77f99cacea3bdf9736d79dc01c997f9afb
jmuraski <muraskijj@hotmail.com>
2012-10-21 13:56:44 -0500

    created rails project

---
+++ wolverine/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wolverine::Application.config.secret_token = '573c4a6d1c6d0648bacc4e2f8d46c8d927008216322ce1d50ab5e4acccaf934dd15567df4155b6a440f555f1ab509f3b4f8eb11047c91b6f3a8585e6fb1a9363'
